34f1be7c70ff4f52d9ed0b2fd8c44b0c8c30b850
El Khamlichi Idriss <idriss.el-khamlichi@student-cs.fr>
2023-11-22 13:55:43 +0100

    ...

--- Flyers/Chat/models.py
+++ Flyers/Chat/models.py
@@ -8,7 +8,8 @@ class Message(models.Model):
     body = models.TextField()
     sent_by = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         ordering = ('created_at',)
@@ -40,13 +41,3 @@ class Room(models.Model):
 
         def __str__(self):
             return f'{self.uuid}'
-
-
-class Sondage(models.Model):
-    question = models.CharField(max_length=1000)
-
-
-class Réponse(models.Model):
-    sondage = models.ForeignKey(
-        Sondage, related_name='reponses', on_delete=models.CASCADE)
-    choix = models.CharField(max_length=100)
--- Flyers/Flyers/urls.py
+++ Flyers/Flyers/urls.py
@@ -16,15 +16,16 @@ urlpatterns = [
     path('api/create-room/<int:eId>/',
          Chat.views.create_room, name='create-room'),
     path('create_event/', Flow.views.createEvent, name='create_event'),
-    #path('login/', Authentication.views.login_page, name='login'),
+    # path('login/', Authentication.views.login_page, name='login'),
     path('login/', Authentication.views.CustomLoginView.as_view(), name='login'),
-    #path('logout/', Authentication.views.logout_user, name='logout'),
+    # path('logout/', Authentication.views.logout_user, name='logout'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('signin/', Authentication.views.register, name='signin'),
     path('GroupPage/', Chat.views.GroupPage, name='GroupPage'),
     path('Room_chat/<int:eId>/', Chat.views.Room_chat, name='Room_chat'),
     path('joinEvent/<int:eId>/', Flow.views.joinEvent, name='joinEvent'),
-    path('JoinEventConfirm/<int:eId>/',Flow.views.JoinEventConfirm, name='JoinEventConfirm'),
+    path('JoinEventConfirm/<int:eId>/',
+         Flow.views.JoinEventConfirm, name='JoinEventConfirm'),
     path('profile/', include('Authentication.urls')),
     path('update-like/', Flow.views.update_like, name='update-like'),
 
@@ -35,4 +36,5 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
